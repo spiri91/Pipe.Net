5e5a87cc33eb7f77489021585d63165562a9fcdc
Ionut Spataru <spataru.ionut91@yahoo.com>
2018-08-01 15:01:41 +0300

    Initial

---
+++ Pipe4Net/PipeIt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pipe4Net
+{
+    public static class PipeIt
+    {
+        public static void Pipe<T>(this T obj, Action<T> action) => action(obj);
+      
+        public static T PipeReturn<T>(this T obj, Func<T, T> func) => func(obj);
+    }
+
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> array, Action<T> action)
+        {
+            foreach (var v in array) action(v);
+        }
+
+        public static T[] DeepCopy<T>(this T[] array)
+        {
+            var length = array.Count();
+
+            var clone = new T[length];
+
+            for (var i = 0; i < length; i++)
+                clone[i] = array[i];
+
+            return clone;
+        }
+
+        public static IEnumerable<T> ShallowCopy<T>(this IEnumerable<T> array) => array;
+
+        public static bool AreSameByValue<T>(this IEnumerable<T> array, IEnumerable<T> arrayToCompare) where T : IEqualityComparer<T>
+        {
+            if (array.Count() != arrayToCompare.Count()) return false;
+
+            foreach (var v in array)
+            {
+                foreach(var w in arrayToCompare)
+                {
+                    if(v.Equals(w)) break;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AreSameByValueAndIndex<T>(this IEnumerable<T> array, IEnumerable<T> arrayToCompare) where T : IEqualityComparer<T>
+        {
+            if (array.Count() != arrayToCompare.Count()) return false;
+
+            for (var i = 0; i < array.Count(); i++)
+            {
+                if(array.ElementAt(i).Equals(arrayToCompare.ElementAt(i)))
+                    break;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool AreSameByReference<T>(this IEnumerable<T> array, IEnumerable<T> arrayToCompare) => ReferenceEquals(array, arrayToCompare);
+    }
+}
---
+++ Pipe4NetInAction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pipe4Net;
+
+namespace Pipe4NetInAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baba = "Baba";
+
+            baba.Pipe(Write);
+
+            baba.PipeReturn(WriteR).Pipe(Write);
+
+            IList<int> aa = new List<int>();
+
+            Console.ReadKey();
+
+            IEnumerable<int> bb = new List<int>();
+        }
+
+        static void Write(string val)
+        {
+            Console.WriteLine(val);
+        }
+
+        static string WriteR(string val)
+        {
+            Console.WriteLine(val);
+
+            return val;
+        }
+    }
+}
